a6eead48a85b12f77741c36eb5b0a5c29d62a167
chirag3084 <110027137+chirag3084@users.noreply.github.com>
2023-02-25 09:52:00 +0530

    Update first.js

--- first.js
+++ first.js
@@ -13,7 +13,7 @@ function lazyLoad() {
 window.addEventListener('scroll', lazyLoad);
 Make your website responsive: Your website should look good and function well on different devices, including desktops, tablets, and smartphones. You can use JavaScript to create responsive navigation menus, adjust the layout based on the screen size, and optimize the images and videos for different devices.
 javascript
-Copy code
+//Copy code
 const toggleBtn = document.querySelector('.toggle-btn');
 const navMenu = document.querySelector('.nav-menu');
 
@@ -22,7 +22,7 @@ toggleBtn.addEventListener('click', () => {
 });
 Use animations and visual effects sparingly: Animations and visual effects can make your website more engaging, but they can also slow down your website and distract users from the content. Use them sparingly and make sure they enhance the user experience rather than detract from it. You can use JavaScript to create animations and visual effects, such as scrolling animations, hover effects, and sliders.
 javascript
-Copy code
+//Copy code
 const slider = document.querySelector('.slider');
 const prevBtn = document.querySelector('.prev-btn');
 const nextBtn = document.querySelector('.next-btn');
@@ -46,7 +46,7 @@ prevBtn.addEventListener('click', () => {
 });
 Make your website accessible: Your website should be accessible to everyone, regardless of their abilities. You can use JavaScript to add keyboard navigation, improve the contrast and readability of the content, and provide alternative text for images and videos.
 javascript
-Copy code
+//Copy code
 const menuLinks = document.querySelectorAll('.menu-link');
 
 menuLinks.forEach((link) => {
@@ -59,7 +59,7 @@ menuLinks.forEach((link) => {
 });
 Test your website: Before launching your website, make sure to test it thoroughly on different browsers and devices. You can use JavaScript to automate the testing process and catch any errors or issues.
 javascript
-Copy code
+//Copy code
 const form = document.querySelector('form');
 const emailInput = document.querySelector('#email');
 const messageInput = document.querySelector('#message');
